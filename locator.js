505238168a418626bd1edeb24e973c830e6bac1b
Manuel Strenge <manuel@strenge.ch>
2018-10-22 13:42:28 +0200

    Added the map

--- locator.js
+++ locator.js
@@ -1,6 +1,13 @@
 
-
+//IMPORTANT: this needs to be called after the container div
 navigator.geolocation.getCurrentPosition(showPosition);
+let mymap = L.map('mapId').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.outdoors',
+    accessToken: 'pk.eyJ1IjoidGhlYnJvbW8iLCJhIjoiY2puazdvZTVrMGNwaTNwcGtya2pnamNxeCJ9.uqohf1YKCWFmba-2iE_67Q'
+}).addTo(mymap);
 
 /**
  * Needs permission to work.
@@ -15,7 +22,6 @@ function showPosition(position){
 }
 
 /**
- * if
+ * if the advanced SKU is used:
  * https://developers.google.com/maps/documentation/embed/usage-and-billing
  */
-/
